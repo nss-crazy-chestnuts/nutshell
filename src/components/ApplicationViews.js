ee9e9036ac68e8f6f35acbc545ee35f331afd47d
Dominic.W.Kantrude <dominic.kantrude52@yahoo.com>
2019-03-06 10:45:20 -0600

    further boilerplate

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,11 +1,84 @@
 import React, { Component } from "react"
 import { Route } from "react-router-dom"
+import EventList from "./event/EventList"
+import NewsList from "./news/NewsList"
+import TasksList from "./task/TaskList"
+import ChatList from "./chat/ChatList"
+import FriendsList from "./Friends/FriendsLIst"
+
 class ApplicationViews extends Component {
-  state = {}
-  componentDidMount() {}
+  state = {
+    events: [],
+    news: [],
+    tasks: [],
+    chat: [],
+    friends: []
+  }
+  componentDidMount() {
+
+
+    const newState = {}
+    newState.events = {}
+    newState.news = {}
+    newState.tasks = {}
+    newState.chat = {}
+    newState.friends = {}
+
+    this.setState(newState)
+
+  }
   render() {
+
+    //isAuthenticated = () => sessionStorage.getItem("credentials") !== null
     console.log(this.props.activeUser)
-    return <React.Fragment />
+
+
+
+    return <React.Fragment>
+
+
+      <Route exact path="/events" render={props => {
+        // if (this.isAuthenticated()) {
+        return <EventList />
+        // } else {
+        //   return <Redirect to="/login" />
+        // }
+      }} />
+      <Route exact path="/news" render={props => {
+        // if (this.isAuthenticated()) {
+        return <NewsList />
+        // } else {
+        //   return <Redirect to="/login" />
+        // }
+      }} />
+      <Route exact path="/tasks" render={props => {
+        // if (this.isAuthenticated()) {
+        return <TasksList />
+        // } else {
+        //   return <Redirect to="/login" />
+        // }
+      }} />
+      <Route exact path="/chat" render={props => {
+        // if (this.isAuthenticated()) {
+        return <ChatList />
+        // } else {
+        //   return <Redirect to="/login" />
+        // }
+      }} />
+      <Route exact path="/friends" render={props => {
+        // if (this.isAuthenticated()) {
+        return <FriendsList />
+        // } else {
+        //   return <Redirect to="/login" />
+        // }
+      }} />
+
+
+    </React.Fragment>
+
+
+
+
   }
 }
 
